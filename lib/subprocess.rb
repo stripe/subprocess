6be0f9280a51a47feeded1f91018e24ba7cb8e53
Nelson Elhage <nelhage@nelhage.com>
2017-11-06 14:01:38 -0800

    recreate sigchld pipe on fork

--- lib/subprocess.rb
+++ lib/subprocess.rb
@@ -560,6 +560,7 @@ module Subprocess
     @sigchld_old_handler = nil
     @sigchld_global_write = nil
     @sigchld_global_read = nil
+    @sigchld_pipe_pid = nil
 
     def self.handle_sigchld
       # We'd like to just notify everything in `@sigchld_fds`, but
@@ -602,7 +603,8 @@ module Subprocess
       @sigchld_mutex.synchronize do
         @sigchld_fds[pid] = fd
         if @sigchld_fds.length == 1
-          if @sigchld_global_write.nil?
+          if @sigchld_global_write.nil? || @sigchld_pipe_pid != ::Process.pid
+            @sigchld_pipe_pid = ::Process.pid
             @sigchld_global_read, @sigchld_global_write = IO.pipe
           end
           @sigchld_old_handler = Signal.trap('SIGCHLD') {handle_sigchld}
